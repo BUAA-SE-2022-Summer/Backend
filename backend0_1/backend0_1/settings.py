101a42afe655c4058c76448d20d1aaf89386b08a
Horik <hr.mail.qaq@gmail.com>
2022-07-28 02:57:42 +0800

    [impl] 本地后端连接云数据库

--- backend0_1/backend0_1/settings.py
+++ backend0_1/backend0_1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend0_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_backend',
+        'USER': 'horik_database_account',
+        'PASSWORD': 'ZAL4CQx2mPLjx+d',
+        'HOST': 'rm-2zeyx2vi320r79mdsio.mysql.rds.aliyuncs.com',
+        'PORT': '3306',
     }
 }
 
